8540f2f4735a773c0e9781e2f5f561e40c8752a9
Augusto Barco <augustopayza@gmail.com>
2023-02-12 23:29:08 -0400

    feat: fix image

--- src/layouts/WeddingLanding.tsx
+++ src/layouts/WeddingLanding.tsx
@@ -14,7 +14,7 @@ const WeddingLanding = () => {
           alt="Picture of the author"
           width={1920}
           height={1080}
-          className="h-screen w-full object-cover"
+          className="h-screen w-full object-cover object-top"
         />
         <div className="absolute bottom-0 h-3/5 w-full items-center bg-gradient-to-t from-black to-transparent p-10">
           <p
