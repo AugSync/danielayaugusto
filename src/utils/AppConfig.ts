a4263e01466768dc154a851f94b815179a4a09b1
Augusto Barco <augustopayza@gmail.com>
2023-02-17 00:53:32 -0400

    feat: add favicon

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 // FIXME: Update this configuration file based on your project information
 
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
-  locale: 'en',
+  site_name: 'Augusto y Daniela',
+  title: 'Augusto y Daniela',
+  description: 'Boda de Augusto y Daniela',
+  locale: 'es',
 };
