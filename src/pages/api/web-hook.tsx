78b3876cea091a0cfb37135414386cb77ded16b5
Augusto Barco <augustopayza@gmail.com>
2023-02-18 00:56:26 -0400

    feat: add webhook

---
+++ src/pages/api/web-hook.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable no-console */
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const auth = {
+    login: process.env.HOOK_LOGIN,
+    password: process.env.HOOK_PASSWORD,
+  }; // change this
+
+  // parse login and password from headers
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+
+  // Verify login and password are set and correct
+  if (login && password && login === auth.login && password === auth.password) {
+    // Access granted...
+    if (!req.body?.previous_entity.attributes.attending && req.body?.entity.attributes.attending) {
+      const rawResponse = await fetch(
+        'https://api.telegram.org/bot6012018939:AAEOB6Jm4GroUgZ-f6to6lz4vSuiSoJoYwM/sendMessage',
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            chat_id: process.env.CHATS_TO_UPDATE,
+            text: `<b>${req.body.entity.attributes.name}</b> ha confirmado la invitación
+          
+<b>Numero de invitados:</b> ${req.body.entity.attributes.number_of_invites}
+<b>Discurso biblico:</b> ${req.body.entity.attributes.speech ? 'Sí asistirá' : 'No asistirá'}
+<b>Sesion de fotos:</b> ${req.body.entity.attributes.photos ? 'Sí asistirá' : 'No asistirá'}
+<b>Compartir:</b> ${req.body.entity.attributes.share ? 'Sí asistirá' : 'No asistirá'}`,
+            parse_mode: 'HTML',
+          }),
+        }
+      );
+      const content = await rawResponse.json();
+
+      res.status(200).json({ body: content });
+    } else res.status(200).json({ body: req.body });
+  } else res.status(400).json({ message: 'Get out of here' });
+}
