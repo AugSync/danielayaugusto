cd14dfec4470f10d5ca0f7ef7b050474e76f5428
Augusto Barco <augustopayza@gmail.com>
2023-02-18 00:14:04 -0400

    feat: add script to generate invit

---
+++ src/scripts/get-invits.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable camelcase */
+/* eslint-disable no-console */
+const path = require('path');
+require('dotenv').config({
+  path: path.resolve(__dirname, '../../', `.env.local`),
+});
+const fs = require('fs');
+const { buildClient } = require('@datocms/cma-client-node');
+const _ = require('lodash');
+
+const client = buildClient({ apiToken: process.env.NEXT_DATOCMS_API_TOKEN });
+
+async function run() {
+  fs.unlinkSync(path.resolve(__dirname, '../../', `invits.txt`));
+
+  // this iterates over every page of results:
+  for await (const {
+    name,
+    number_of_invites,
+    invitation_code,
+    url_completa,
+  } of client.items.listPagedIterator()) {
+    if (name) {
+      console.log({ name, number_of_invites, invitation_code, url_completa });
+
+      fs.appendFileSync(
+        path.resolve(__dirname, '../../', `invits.txt`),
+        `
+    
+${_.trim(name)}, ${
+          number_of_invites > 1
+            ? 'queremos que estén en nuestra boda'
+            : 'queremos que estés en nuestra boda'
+        }. 
+
+${url_completa?.replace('danielayaugusto.vercel.app', 'augustoydaniela.com')}
+
+Código de invitación: ${invitation_code}
+    `
+      );
+    }
+  }
+}
+
+run();
